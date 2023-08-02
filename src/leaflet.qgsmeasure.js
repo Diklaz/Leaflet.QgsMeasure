a9d1b0d0b38c11ba555457a9c014989a365f1177
Gabriel Russo <gabrielrusso@protonmail.com>
2023-08-02 11:54:36 -0400

    Control box in map, refactor code structure

--- src/leaflet.qgsmeasure.js
+++ src/leaflet.qgsmeasure.js
@@ -1,168 +1,32 @@
 import * as L from "leaflet";
-import "leaflet-draw/dist/leaflet.draw-src";
-import "leaflet-draw/dist/leaflet.draw-src.css";
+import "./leaflet.polyline.measure.js";
 import "./leaflet.qgsmeasure.css";
-
-L.Polyline.Measure = L.Draw.Polyline.extend({
-  addHooks() {
-    L.Draw.Polyline.prototype.addHooks.call(this);
-    if (this._map) {
-      this._markerGroup = new L.LayerGroup();
-      this._map.addLayer(this._markerGroup);
-
-      this.options.shapeOptions = {
-        color: "#d07f03",
-        stroke: true,
-        weight: 4,
-        opacity: 0.7,
-        fill: false,
-        clickable: true,
-      };
-
-      this.options.icon = new L.DivIcon({
-        iconSize: new L.Point(9, 9),
-        className: 'leaflet-div-icon leaflet-editing-icon',
-      });
-
-      this._segments = [];
-      this._markers = [];
-      this._map.on('click', this._onClick, this);
-      this._startShape();
-    }
-  },
-
-  removeHooks() {
-    L.Draw.Polyline.prototype.removeHooks.call(this);
-
-    this._clearHideErrorTimeout();
-
-    // !\ Still useful when control is disabled before any drawing (refactor needed?)
-    this._map
-      .off('pointermove', this._onMouseMove, this)
-      .off('mousemove', this._onMouseMove, this)
-      .off('click', this._onClick, this);
-
-    this._clearGuides();
-    this._container.style.cursor = '';
-
-    this._removeShape();
-  },
-
-  _startShape() {
-    this._drawing = true;
-
-    this._map.fire("qgsmeasure:measurestart");
-
-    this._poly = new L.Polyline([], this.options.shapeOptions);
-
-    // this is added as a placeholder, if leaflet doesn't receive
-    // this when the tool is turned off all onclick events are removed
-    this._poly._onClick = () => {
-    };
-
-    this._container.style.cursor = 'crosshair';
-
-    this._updateTooltip();
-
-    this._map
-      .on('pointermove', this._onMouseMove, this)
-      .on('mousemove', this._onMouseMove, this);
-  },
-
-  _finishShape() {
-    this._drawing = false;
-
-    this._cleanUpShape();
-    this._clearGuides();
-
-    this._updateTooltip();
-
-    this._map
-      .off('pointermove', this._onMouseMove, this)
-      .off('mousemove', this._onMouseMove, this);
-
-    this._container.style.cursor = '';
-  },
-
-  _removeShape() {
-    if (!this._poly) return;
-    this._map.removeLayer(this._poly);
-    delete this._poly;
-    this._markers.splice(0);
-    this._markerGroup.clearLayers();
-    this._clearSegments();
-  },
-
-  _onClick(e) {
-    if (this._markers.length > 1 && this._drawing) {
-      this._addSegment();
-      this._updateSegmentTooltipNumber();
-    }
-
-    if (!this._drawing) {
-      this._removeShape();
-      this._startShape();
-    }
-  },
-
-  getSegments() {
-    return this._segments;
-  },
-
-  _updateSegmentTooltipNumber() {
-    let i = 1;
-    for (const marker of this._markers) {
-      marker.bindTooltip(`${i}`, {
-        permanent: true,
-        offset: L.point(6, 0),
-        opacity: 1,
-      })
-        .openTooltip();
-
-      i += 1;
-    }
-  },
-
-  _addSegment() {
-    let A = this._markers.at(-2)
-      .getLatLng();
-
-    let B = this._markers.at(-1)
-      .getLatLng();
-
-    const segmentDistance = this._map.distance(A, B);
-
-    this._segments.push({
-      from: this._markers.length - 1,
-      to: this._markers.length,
-      distance: segmentDistance,
-    });
-
-    this._map.fire("qgsmeasure:newsegment", { segments: this._segments });
-  },
-
-  _clearSegments() {
-    this._segments = [];
-    this._map.fire("qgsmeasure:measurestop");
-  },
-
-  _getTooltipText() {
-    let labelText = L.Draw.Polyline.prototype._getTooltipText.call(this);
-    if (!this._drawing) {
-      labelText.text = '';
-    }
-    return labelText;
-  },
-});
 
 L.Control.QgsMeasure = L.Control.extend({
 
   statics: {
     TITLE: 'Measure distances',
+    SEGMENTS_TITLE: 'Segments (meters):',
+    SEGMENTS_FROM: "From: ",
+    SEGMENTS_TO: "to: ",
+    SEGMENTS_TOTAL: 'Total: ',
+    SEGMENTS_METERS: "m",
   },
   options: {
     position: 'topleft',
     handler: {},
+    shapeOptions: {
+      color: "#d07f03",
+      stroke: true,
+      weight: 4,
+      opacity: 0.7,
+      fill: false,
+      clickable: true,
+    },
+    icon: new L.DivIcon({
+      iconSize: new L.Point(9, 9),
+      className: 'leaflet-div-icon leaflet-editing-icon',
+    }),
   },
 
   toggle() {
@@ -177,22 +41,91 @@ L.Control.QgsMeasure = L.Control.extend({
     return this.handler.getSegments();
   },
 
+  initialize(options) {
+    L.Util.setOptions(this, options);
+  },
+
+  _createTextElement(tag, className, container, text) {
+    L.DomUtil.create(tag, className, container).innerText = text;
+  },
+
+  _createSegmentContainer() {
+    if (this._segments_container) {
+      return;
+    }
+
+    /* Getting the leaflet top right container */
+    const [topleft] = document.getElementsByClassName("leaflet-top leaflet-right");
+
+    /* Box over the map containing all data */
+    this._segments_container = L.DomUtil.create('div', `segments-container leaflet-control`, topleft);
+
+    /* Prevents zoom map when scroll over the container */
+    L.DomEvent.disableScrollPropagation(this._segments_container);
+
+    /* Box Title */
+    this._createTextElement('span', 'segments-title', this._segments_container, L.Control.QgsMeasure.SEGMENTS_TITLE);
+
+    /* Box containing all the measures */
+    this._segments_measures_container = L.DomUtil.create('div', "segments-measures-container", this._segments_container);
+
+    /* Box containing the Sum of distances (it's separate from measures to stay on bottom) */
+    this._segments_total_distance_container = L.DomUtil.create('div', "segments-total-distance-container", this._segments_container);
+
+    /* Initialize total distance text */
+    this._total_distance_text_reset = `${L.Control.QgsMeasure.SEGMENTS_TOTAL} 0 ${L.Control.QgsMeasure.SEGMENTS_METERS}`;
+    this._createTextElement('span', '', this._segments_total_distance_container, this._total_distance_text_reset);
+    this._total_distance_text_element = document.getElementsByClassName('segments-total-distance-container')[0].firstChild;
+  },
+
+  _startMeasure(e) {
+    let { segments } = e;
+    const lastSegment = segments.at(-1);
+
+    const from = L.Control.QgsMeasure.SEGMENTS_FROM;
+    const to = L.Control.QgsMeasure.SEGMENTS_TO;
+    const meters = L.Control.QgsMeasure.SEGMENTS_METERS;
+
+    let totalDistance = segments.reduce((acc, segment) => acc + segment.distance, 0);
+
+    const text = `${from} ${lastSegment.from} ${to} ${lastSegment.to} = ${lastSegment.distance.toFixed(2)} ${meters}`;
+
+    this._createTextElement('span', 'segment-measure', this._segments_measures_container, text);
+
+    this._total_distance_text_element.innerText = `${L.Control.QgsMeasure.SEGMENTS_TOTAL} ${totalDistance.toFixed(3)} ${L.Control.QgsMeasure.SEGMENTS_METERS}`;
+
+    this._segments_container.scrollTop = this._segments_measures_container.scrollHeight;
+  },
+
+  _finishMeasure() {
+    while (this._segments_measures_container.firstChild) {
+      this._segments_measures_container.removeChild(this._segments_measures_container.firstChild);
+    }
+    this._total_distance_text_element.innerText = this._total_distance_text_reset;
+  },
+
   onAdd(map) {
     let link = null;
     let className = 'leaflet-control-draw';
 
     this._container = L.DomUtil.create('div', 'leaflet-bar');
 
-    this.handler = new L.Polyline.Measure(map, this.options.handler);
+    this.handler = new L.Polyline.Measure(map, this.options);
 
+    this._map.on("qgsmeasure:measurestart", this._createSegmentContainer, this);
+    this._map.on("qgsmeasure:newsegment", this._startMeasure, this);
+    this._map.on("qgsmeasure:measurestop", this._finishMeasure, this);
+
+    /* Leaflet Control */
     this.handler.on('enabled', () => {
+      L.DomUtil.addClass(this._segments_container, 'show');
       this.enabled = true;
-      L.DomUtil.addClass(this._container, 'enabled');
     }, this);
 
     this.handler.on('disabled', () => {
       delete this.enabled;
-      L.DomUtil.removeClass(this._container, 'enabled');
+      L.DomUtil.removeClass(this._segments_container, 'show');
+      this._finishMeasure();
     }, this);
 
     link = L.DomUtil.create('a', `${className}-measure`, this._container);
